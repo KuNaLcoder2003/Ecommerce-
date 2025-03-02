314f1b0d599612435c1de8a214131ed7938d28b5
Kunal Singh <kunalindia59@gmail.com>
2025-03-02 21:30:23 +0530

    a new router

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const isAdmin = require('../middlewares/isAdmin')
-const {products_schema} = require('../types')
+const authMiddleware = require('../middlewares/auth')
+const { products_schema } = require('../types')
 const router = express.Router();
-const {Users , Products} = require('../db')
-const upload = require('../functions/cloudinary')
+const { Users, Products } = require('../db')
+const { uploadImages, deleteImages } = require('../functions/cloudinary')
 
-router.post('/'   ,  async(req , res)=>{
-    const {product_name , category , product_description , quantity , price , product_images} = req.body;
+router.post('/', async (req, res) => {
+    const { product_name, category, product_description, quantity, price, product_images } = req.body;
     const userId = req.userId;
     const role = req.role
     try {
-        // console.log('1')
-        const {success} = products_schema.safeParse({
+        const { success } = products_schema.safeParse({
             product_name,
             category,
             product_description,
@@ -19,40 +19,123 @@ router.post('/'   ,  async(req , res)=>{
             price,
             product_images
         })
-        // console.log('2');
 
-        if(!success){
-            // console.log('3')
+
+        if (!success) {
+
             return res.status(403).json({
-                message : 'Insufficient information',
+                message: 'Insufficient information',
             })
         }
-        // console.log('4')
+
+        const result = await uploadImages(product_images, product_name)
+        console.log(result)
+
+        let arr = result.map((obj) => {
+            return { url: obj.url, public_id: obj.public_id }
+        })
+        console.log(arr);
         const product = new Products({
             product_name,
             category,
             product_description,
             quantity,
             price,
-            product_images
+            product_images: arr
         })
-        // console.log('5')
-        upload(product_images[0].url , 'image')
+
         await product.save();
 
-        // console.log('6')
+
         return res.status(200).json({
-            message : 'Successfully added a new product'
+            message: 'Successfully added a new product'
         })
-        
+
     } catch (error) {
-        // console.log('7')
+
         res.status(500).json({
-            message : 'Something went wrong'
+            message: 'Something went wrong'
         })
     }
 })
 
+router.get('/:productId', authMiddleware, async (req, res) => {
+    try {
+        const productId = req.params.productId
+        if (productId.length < 3) {
+            return res.status(401).json({
+                message: 'Bad request'
+            })
+        }
+        const product = await Products.findOne({ _id: productId })
+        if (!product) {
+            return res.status(402).json({
+                message: 'No product found'
+            })
+        }
+        return res.status(200).json({
+            product
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Something went wrong'
+        })
+    }
+})
+
+
+router.get('/:category', authMiddleware, async (req, res) => {
+    const category = req.params.category;
+    try {
+        const products = await Products.find({
+            category: category
+        })
+        if (products.length == 0) {
+            return res.status(404).json({
+                message: 'No products found'
+            })
+        }
+        res.status(200).json([
+            products
+        ])
+    } catch (error) {
+        res.status(500).json({
+            message: 'Something went wrong'
+        })
+    }
+})
+
+router.delete('/:productId', async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        const product = await Products.findOne({ _id: productId });
+        if (!product) {
+            return res.status(402).json({
+                message: 'product doesnot exists'
+            })
+        }
+        const results = await deleteImages(product.product_images)
+        // console.log(results);
+        if (results.length == 0) {
+            return res.status(402).json({
+                message: 'Error deleting product'
+            })
+        }
+        const e = results.filter(obj => obj.result !== 'ok');
+        if (e.length > 0) {
+            return res.status(402).json({
+                message: 'Error deleting product'
+            })
+        }
+
+        await product.deleteOne()
+        res.status(200).json({
+            message: 'Successfully deleted product'
+        })
+    } catch (error) {
+
+    }
+})
 
 
 module.exports = router
