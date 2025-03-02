3c946f8679f685a76cae6051ed51482c58ac6d6c
Kunal Singh <kunalindia59@gmail.com>
2025-03-02 21:32:20 +0530

    added a zod schema for input validation of products

--- backend/types.js
+++ backend/types.js
@@ -5,7 +5,8 @@ const signup_schema = zod.object({
     first_name : zod.string(),
     last_name : zod.string(),
     username : zod.string().email(),
-    password : zod.string()
+    password : zod.string(),
+    role : zod.string(),
 })
 
 const signin_schema = zod.object({
@@ -13,8 +14,19 @@ const signin_schema = zod.object({
     password : zod.string()
 })
 
+const products_schema = zod.object({
+    product_name : zod.string(),
+    category : zod.string() ,
+    product_description: zod.string(),
+    quantity : zod.number(),
+    price : zod.number(),
+    product_images : zod.array(zod.object({
+        url : zod.string()
+    }))
+})
 
 module.exports = {
     signup_schema,
     signin_schema,
+    products_schema,
 }
