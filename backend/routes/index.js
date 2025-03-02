86ab0e4d9dcf3c4724e4381077ac7a4d294c97dc
Kunal Singh <kunalindia59@gmail.com>
2025-03-02 21:29:39 +0530

    included a router for handling all product related request

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userRouter = require('./user')
+const productRouter = require('./products')
 
 router.use('/user' , userRouter)
-
+router.use('/product' , productRouter);
 
 module.exports = router
