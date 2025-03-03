c1f643f3584b275db002869374d4d06bc68b87ac
Kunal Singh <kunalindia59@gmail.com>
2025-03-03 23:11:05 +0530

    loading the order route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userRouter = require('./user')
 const productRouter = require('./products')
+const orderRouter = require('./order')
 
 router.use('/user' , userRouter)
 router.use('/product' , productRouter);
-
+router.use('/order' , orderRouter);
 module.exports = router
