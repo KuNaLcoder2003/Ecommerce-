0fdb0a020afadb04989bd82f4544a04c3a4d476b
Kunal Singh <kunalindia59@gmail.com>
2025-03-03 23:13:55 +0530

    aaded a route for all cart related request

---
+++ backend/routes/Cart.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth');
+const router = express.Router();
+
+router.post('/' , async(req , res)=>{
+    // const userId = req.userId;
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+
+module.exports = router
