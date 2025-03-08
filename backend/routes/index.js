3d33a231a879b518e660d4b9cc4b6e79f887cbbf
Kunal Singh <kunalindia59@gmail.com>
2025-03-08 17:04:52 +0530

    added carts route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userRouter = require('./user')
-const productRouter = require('./products')
+const productRouter = require('./product')
 const orderRouter = require('./order')
+const cartRouter = require('./cart')
 
 router.use('/user' , userRouter)
 router.use('/product' , productRouter);
 router.use('/order' , orderRouter);
+router.use('/cart' , cartRouter)
 module.exports = router
