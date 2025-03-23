e466495923af79641f79e9d4a4580920885a8121
Kunal Singh <kunalindia59@gmail.com>
2025-03-24 00:59:57 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 
 app.use('/api/v1' , mainRouter)
 
+app.use(express.json({ limit: "50mb" }));  // Increase JSON payload size
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase URL-encoded data size
+
+
 app.listen(3000 , ()=>{
     console.log('App started');
 })
