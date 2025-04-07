aea3418a01e6ef5cfadbdf8f262aa934d9f6a5e3
Kunal Singh <kunalindia59@gmail.com>
2025-04-07 21:29:47 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,16 @@ const cors = require('cors');
 
 app.use(cors());
 
+
+// app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
 app.use('/api/v1' , mainRouter)
 
-app.use(express.json({ limit: "50mb" }));  // Increase JSON payload size
-app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase URL-encoded data size
+// app.use(express.json({ limit: "50mb" }));  // Increase JSON payload size
+ // Increase URL-encoded data size
 
 
 app.listen(3000 , ()=>{
